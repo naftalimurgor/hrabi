f146855da16b7899eb87cf6852c54454125cae0a
naftalimurgor <murgornaftali@gmail.com>
2022-03-07 13:04:32 +0300

    Feat: add Cli program

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+
+import { Command } from 'commander'
+import Parser from './Parser'
+
+const program = new Command()
+program
+  .name('hrabi')
+  .description('hrabi- JSON ABI to Human Readable ABI parser')
+  .version('0.1.0')
+
+program.command('parse')
+  .argument('--abi')
+  .argument('--output')
+  .action((abi, output) => {
+    Parser.parse(abi, output)
+  })
+program.parse()
